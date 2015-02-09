2f0839a346a87d0416812d0d0eb74ba75bc0d624
booster085 <iliangrekov@abv.bg>
2015-02-09 16:37:01 +0200

    Deleted same unnecessary files

--- BostonPongGame/src/game/Sound.java
+++ BostonPongGame/src/game/Sound.java
@@ -5,7 +5,7 @@ import java.applet.AudioClip;
 
 public class Sound {
 	
-	public static final AudioClip HIT = Applet.newAudioClip(Sound.class.getResource("sound/hit3.wav"));	
+	public static final AudioClip HIT = Applet.newAudioClip(Sound.class.getResource("sound/hit.wav"));	
 	public static final AudioClip MUSIC = Applet.newAudioClip(Sound.class.getResource("sound/music.wav"));
 	public static final AudioClip POINT = Applet.newAudioClip(Sound.class.getResource("sound/point.wav"));
 	
